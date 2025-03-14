4f12551e1f5647e3c0cc867e6573dffba7baabf8
naveenpradeep0987@gmail.com <Naveenpradeep02.noreply.github.com>
2025-03-14 12:09:43 +0530

    update

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ function toggleFAQ(id) {
   answer.classList.toggle("hidden");
   currentOpen = answer.classList.contains("hidden") ? null : id;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  function toggleFixedSection() {
+    const firstSection = document.getElementById("first-section");
+    const fixedSection = document.getElementById("fixedSection");
+    console.log(firstSection);
+    console.log(fixedSection);
+
+    if (!firstSection || !fixedSection) {
+      console.error("Element not found!");
+      return; // Stop execution if elements are missing
+    }
+
+    const sectionHeight = firstSection.offsetHeight;
+    const scrolled = window.scrollY;
+
+    if (scrolled > sectionHeight * 0.3) {
+      fixedSection.classList.add("visible-section");
+      fixedSection.classList.remove("hidden-section");
+    } else {
+      fixedSection.classList.add("hidden-section");
+      fixedSection.classList.remove("visible-section");
+    }
+  }
+
+  window.addEventListener("scroll", toggleFixedSection);
+});
